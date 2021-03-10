a7d44c3810908d923220290445e2a6cab5b0a87a
JonaszMx <>
2021-03-09 21:11:36 -0600

    Se agrega cosntante que valida el tipo de nombramiento alta provisional.

--- src/main/java/constants/ConstantsWS.java
+++ src/main/java/constants/ConstantsWS.java
@@ -12,4 +12,6 @@ package constants;
 public class ConstantsWS {
     public static final String LLAVE_CIFRADO = "U2FsdGVkX1/7QtF2CTaJm2b1JwhUwPrQ6Yvk4+iiwSk="; //MasterKey
     public static final String BITACORA_LOGIN = "sp_insertBitacoraLogin";
+    public static final String ALTA_DEFINITIVA = "5";
+    public static final String ALTA_PROVICIONAL = "3";
 }
