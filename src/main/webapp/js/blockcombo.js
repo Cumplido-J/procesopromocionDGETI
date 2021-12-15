71bbd3512188b49aa79112dfa4314fa573656272
Juan <ortegacumplidojuanjose1@gmail.com>
2021-12-15 14:17:05 -0600

    correcion de entidad y plantel en usuarios

--- src/main/webapp/js/blockcombo.js
+++ src/main/webapp/js/blockcombo.js
@@ -11,14 +11,16 @@ function blockCombos() {
     const sub = programa.getAttribute("data-sub");
     const ent = programa.getAttribute("data-ent");
     const pla = programa.getAttribute("data-pla");
-    console.log(sub + " " + pla + " " + " " + ent)
-    if (sub != "" && ent != "" && pla != "") {
+    console.log(sub + " " + pla + " " + " " + ent);
+    if (sub !== "" && ent !== "" && pla !== "") {
+        if (( sub !== null) && ( ent !== null) && ( pla !== null )) {
         $("#subsistema").val(sub).attr('selected', 'selected');
         $("#entidad").val(ent).attr('selected', 'selected');
         $("#plantel").val(pla).attr('selected', 'selected');
         $("#subsistema").addClass("disabled");
         $("#entidad").addClass("disabled");
         $("#plantel").addClass("disabled");
+        }
     }
 
 }
